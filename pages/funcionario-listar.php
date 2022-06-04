1baf3302a0b9511ea453fd6afbadcd10b703f8d1
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2022-06-04 19:05:34 -0300

    Criando listar

--- pages/funcionario-listar.php
+++ pages/funcionario-listar.php
@@ -1,1 +1,49 @@
-<h1>FUNCIONARIO listar</h1>
+<h1>Materiais</h1>
+<div class="tab-pane fade show active" id="cargo-tab-pane" role="tabpanel" aria-labelledby="cargo-tab" tabindex="0">
+                        <table class="table table-striped">
+                            <thead class="table-light">
+                            <div class="row mt-1">
+                                <div class="col">
+                                    
+                                </div>
+                                <div class="col d-flex justify-content-end">
+                                    <button class="btn btn-primary me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-plus" viewBox="0 0 16 16">
+                                            <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z"/>
+                                            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+                                        </svg>
+                                        Cadastrar
+                                    </button>
+                                </div>
+                            </div>
+                              <tr>
+                                <th scope="col"> # </th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">CPF</th>
+                                <th scope="col">Data Nascimento</th>
+                                <th></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>1</td>
+                                <td>Phelipe Afonso</td>
+                                <td>071.505.141-54</td>
+                                <td>13/09/1999</td>
+                                <td class="d-flex justify-content-end p-3">
+                                    <button class="btn btn-primary me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                          </svg>
+                                    </button>
+                                    <button class="btn btn-danger me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                          </svg>
+                                    </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+</div>
--- pages/laudo-listar.php
+++ pages/laudo-listar.php
@@ -1,1 +1,1 @@
-<h1>laudo listar</h1>
+<h1>Laudo</h1>
