4759b7f9822bcb4245ec842e164c1afba7d85b4f
Isaac Alcantara <isaacalcz1@gmail.com>
2022-06-06 01:57:17 -0300

    crud especialidade

--- pages/cargo-listar.php
+++ pages/cargo-listar.php
@@ -1,5 +1,3 @@
-<h1>CARGO LISTAR</h1>
-
 <?php
 
     $sql = 'SELECT * FROM cargo';
--- pages/especialidade-cadastrar.php
+++ pages/especialidade-cadastrar.php
@@ -1,3 +1,5 @@
+<form action='?page=especialidade-salvar' method='POST'>
+<input type='hidden' name='acao' value='cadastrar'>
 <div class='container'>
     <div class='row'>
         <h1>Cadastrar Especialidade</h1>
@@ -9,7 +11,7 @@
                      <path d='M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z'/>
                     </svg>
                 </span>
-                <input type='text' class='form-control' placeholder='Especialidade' aria-label='Especialidade' aria-describedby='basic-addon1' name='especialidade' id='especialidade'>
+                <input type='text' class='form-control' placeholder='Especialidade' aria-label='Especialidade' aria-describedby='basic-addon1' name='nome_especialidade' id='especialidade'>
             </div>
         </div>
     </div>
@@ -32,6 +34,6 @@
         </div>
     </div>
 </div>
-
+</form>
 
 
